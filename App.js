40d4525dd9c25303c6b39d3ba9c2fbbf27dd3ea3
Alger Bucag <algerbucag3@gmail.com>
2023-11-18 11:52:26 +0800

    init commit

---
+++ App.js
@@ -0,0 +1,59 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { Login, Signup, Welcome,Forgot, Reset,TodoList } from "./screens";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Welcome'
+      >
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false
+          }}     
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="Forgot"
+          component={Forgot}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="Reset"
+          component={Reset}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="TodoList"
+          component={TodoList}
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
